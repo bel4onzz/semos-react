4b3704b79c58a34ad798698495c6d5d0c1e84736
hristijankostadinoski <hristijan@2coders.com>
2024-09-23 20:30:12 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  console.log("render")
+
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <p>Welcome to the React 18 app!</p>
+    </div>
+  )
+}
+
+export default App
